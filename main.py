3371220496d546e1bd928b98f99ba862d47bd5ca
Gabben <43146729+gabbhack@users.noreply.github.com>
2022-01-26 22:09:01 +0500

    Get title from env

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ async def main():
     md_content = Path("README.md").read_text("utf-8")
     html_content = html(md_content)
     nodes_content = list(filter(lambda x: x != "\n", html_to_nodes(html_content)))
-    await telegraph.edit_page(os.environ["TELEGRAPH_PATH"], title="aiogram [ru]", content=nodes_content,
+    await telegraph.edit_page(os.environ["TELEGRAPH_PATH"], title=os.environ["TELEGRAPH_TITLE"], content=nodes_content,
                               author_name=os.environ["TELEGRAPH_AUTHOR"], author_url=os.environ["TELEGRAPH_AUTHOR_URL"])
 
     await telegraph.close()
